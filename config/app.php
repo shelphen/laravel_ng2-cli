c82ab38552cb7499437167a0362bf615df585e76
Chaniwat Seangchai <chaniwat.meranote@gmail.com>
2016-11-06 16:04:31 +0700

    add jwt-auth

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
          * Package Service Providers...
          */
 
-        //
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -208,6 +208,8 @@ return [
         'File' => Illuminate\Support\Facades\File::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
