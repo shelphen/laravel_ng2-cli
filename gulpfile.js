6009b3762fb8cde94964dd483ebb07827fe24949
Chaniwat Seangchai <chaniwat.meranote@gmail.com>
2016-11-18 05:10:26 +0700

    -_- fix gulpfile (2)

--- gulpfile.js
+++ gulpfile.js
@@ -14,18 +14,3 @@ gulp.task('cpybuild', () => {
   copydir.sync('out_dist/', 'public_html/');
   del.sync(['out_dist/**/*']);
 });
-
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for your application as well as publishing vendor resources.
- |
- */
-
-elixir((mix) => {
-
-});
